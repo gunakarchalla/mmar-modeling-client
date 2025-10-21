9543e25f1866fc0f92774b89600a540f7e67c75e
Gunakar Challa <gunakarchalla@gmail.com>
2025-10-21 09:38:53 +0000

    fixed slider bug for no default values and 0 not in range

--- src/resources/services/value_converters.ts
+++ src/resources/services/value_converters.ts
@@ -2,10 +2,10 @@ import { valueConverter } from 'aurelia';
 
 @valueConverter('numerise')
 export class NumeriseConverter {
-    toView(value: string): number {
-        if (value == 'not defined' || value == 'undefined' || value == '') return 0;
-        if (!value) return 0;
-        return parseFloat(value) || 0;
+    toView(value: string, default_value: number, fallback_value: number): number {
+        if (value == 'not defined' || value == 'undefined' || value == '') return default_value ? default_value : fallback_value;
+        if (!value) return default_value ? default_value : fallback_value;
+        return parseFloat(value);
     }
     fromView(value: number): string {
         return value.toString();
